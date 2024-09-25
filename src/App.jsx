4dcbd4538e286ea97781f033f7a337fec9f76987
Vo Huy Hung <22520528@gm.uit.edu.vn>
2024-09-25 17:50:36 +0700

    Add view all Job coponent

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,16 @@ import NavBar from "./component/NavBar";
 import Hero from "./component/Hero";
 import HomeCard from "./component/HomeCard";
 import JobListing from "./component/JobListing";
+import ViewAllJob from "./component/ViewAllJob";
 
 const App = () => {
   return (
     <>
       <NavBar />
-      <Hero tittle="Test tittle" description="It is just a test" />
+      <Hero />
       <HomeCard />
       <JobListing />
-      
-
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >
-          View All Jobs
-        </a>
-      </section>
+      <ViewAllJob />
     </>
   );
 };
--- src/component/JobListing.jsx
+++ src/component/JobListing.jsx
@@ -1,6 +1,9 @@
 import jobs from "../jobs.json";
 import Job from "./Job";
 const JobListing = () => {
+
+  const jobCurrent = jobs.slice(0, 3);
+
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
@@ -8,7 +11,7 @@ const JobListing = () => {
           Browse Jobs
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {jobs.map((job) => (
+          {jobCurrent.map((job) => (
             <Job key={job.id} job={job} />
           ))}
         </div>
