f07fe711f7af326eb83e6d8934c4f30f5bbdf762
Vo Huy Hung <22520528@gm.uit.edu.vn>
2024-09-25 19:34:33 +0700

    add map icon to Location in Job component

--- src/component/Job.jsx
+++ src/component/Job.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { FaMapMarker } from "react-icons/fa";
 
 const Job = ({ job }) => {
   const [showFullDescription, setShowFullDescription] = useState(true);
@@ -17,7 +18,12 @@ const Job = ({ job }) => {
         </div>
 
         <div className="mb-5">{description}</div>
-        <button onClick={()=>{setShowFullDescription((prevState)=>!prevState)}} className="text-indigo-500 mb-5 hover:text-indigo-600">
+        <button
+          onClick={() => {
+            setShowFullDescription((prevState) => !prevState);
+          }}
+          className="text-indigo-500 mb-5 hover:text-indigo-600"
+        >
           {showFullDescription ? "Less" : "More"}
         </button>
 
@@ -27,7 +33,7 @@ const Job = ({ job }) => {
 
         <div className="flex flex-col lg:flex-row justify-between mb-4">
           <div className="text-orange-700 mb-3">
-            <i className="fa-solid fa-location-dot text-lg"></i>
+            <FaMapMarker className="inline text-lg mb-1 mr-1" />
             {job.location}
           </div>
           <a
